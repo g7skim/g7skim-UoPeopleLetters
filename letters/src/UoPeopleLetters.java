ae8b102299cbd290ba0d65e4565682c8e9b8169a
g7skim <jamuday@me.com>
2017-07-18 00:00:54 +0300

    Initial

---
+++ letters/src/UoPeopleLetters.java
@@ -0,0 +1,22 @@
+/**
+* The UoPeopleLetters program prints logo of the UoPeople in ascii graphic
+*
+* @author  Eugene Vereshchagin (g7skim)
+* @email   jamuday@me.com
+* @since   2017-07-17 
+*/
+public class UoPeopleLetters {
+    
+       public static void main(String[] args) {
+          System.out.println();
+          System.out.println(" _    _       _____                 _      ");
+          System.out.println("| |  | |     |  __ \\               | |     ");
+          System.out.println("| |  | | ___ | |__) |__  ___  _ __ | | ___ ");
+          System.out.println("| |  | |/ _ \\|  ___/ _ \\/ _ \\| '_ \\| |/ _ \\");
+          System.out.println("| |__| | (_) | |  |  __/ (_) | |_) | |  __/");
+          System.out.println(" \\____/ \\___/|_|   \\___|\\___/| .__/|_|\\___|");
+          System.out.println("                             | |           ");
+          System.out.println("                             |_|           ");
+       }  // end main()
+
+    } 
